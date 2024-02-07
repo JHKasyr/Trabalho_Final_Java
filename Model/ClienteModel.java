5001b4d6e5cb5e48bf19d3014a7f08ba2aa37705
Paulo Natã Jablonski <119634033+Paulo-N-Jablonski@users.noreply.github.com>
2024-02-07 20:23:33 -0300

    feito model e controller
    controller 100% e o model tamo fazendo ainda

---
+++ Model/ClienteModel.java
@@ -0,0 +1,81 @@
+package Model;
+
+public class ClienteModel {
+    
+    int id;
+    String cpf;
+    String nome;
+    String endereço;
+    String dataNascimento;
+    String email;
+    
+    // Construtores
+    public ClienteModel() {
+    }
+
+    public ClienteModel(String cpf, String nome, String endereço, String dataNascimento, String email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.endereço = endereço;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+    }
+
+    public ClienteModel(int id, String cpf, String nome, String endereço, String dataNascimento, String email) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.endereço = endereço;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+    }
+    
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
